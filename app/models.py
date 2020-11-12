6f548ed749683245e6ffd0d7915acedbf19a1d47
root <root@localhost.localdomain>
2020-11-12 10:07:43 +0800

    用户登陆

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -13,7 +16,15 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}, Email {}, Password_Hash {}, Posts {}'.format(self.username, self.email, self.password_hash, self.posts)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
